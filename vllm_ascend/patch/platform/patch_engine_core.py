64b174217ef6dc8cd87d184f16c8dfda8009a7f1
theroadtofree <hw.wangtongtong@huawei.com>
2026-07-25 23:13:37 +0800

    Revert "benchmark freeze fix, Prefill first ZMQ pushlish delaying"
    This reverts commit 6c95dc6b150594b8c4fa8812e2a913e8d9592a23.

--- vllm_ascend/patch/platform/patch_engine_core.py
+++ vllm_ascend/patch/platform/patch_engine_core.py
@@ -620,6 +620,9 @@ def _patched_step_with_batch_queue(self):
 
             if not deferred_scheduler_output:
                 batch_queue.appendleft((future, scheduler_output, exec_future))
+                # [ascend insert] Publish PRE_OUT only when the oldest
+                # head segment becomes next to execute (rightmost in deque).
+                self._publish_pre_out_when_ready()
                 # [ascend insert] Log batch_queue contents for debugging.
                 queue_types = [
                     so.batch_type.value
@@ -643,10 +646,6 @@ def _patched_step_with_batch_queue(self):
         return None, False
 
     # Block until the next result is available.
-    # [ascend insert] Publish PRE_OUT for the head segment that is about
-    # to execute (rightmost in deque).  FIFO guarantees every PREFILL_FIRST
-    # eventually becomes batch_queue[-1] before pop().
-    self._publish_pre_out_when_ready()
     future, scheduler_output, exec_model_fut = batch_queue.pop()
     with (
         self.log_error_detail(scheduler_output),
